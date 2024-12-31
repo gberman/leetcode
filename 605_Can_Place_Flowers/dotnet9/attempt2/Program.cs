6ccdb747d3920bd680966c714151e26a6e54f03a
Gregory Berman <greg@5fn.ai>
2024-12-31 00:35:16 -0500

    completed question 605 Can place flower

---
+++ 605_Can_Place_Flowers/dotnet9/attempt2/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace leetcode;
+public class Solution
+{
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        if (flowerbed.Length == 1 && flowerbed[0] == 0) return --n <= 0;
+
+        for(int i = 0; i < flowerbed.Length; i++)
+        {
+            if (flowerbed[i] == 1) continue;
+            if (i == 0)
+            {
+                if (flowerbed.Length > 2 && flowerbed[i + 1] == 0)
+                {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            } 
+            else if (i == flowerbed.Length - 1)
+            {
+                if (flowerbed.Length >= 2 && flowerbed[i-1] == 0)
+                {
+                    flowerbed[i] = 1;
+                    n--;
+                }
+            }
+            else if (flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0)
+            {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return n <= 0;
+    }
+}
---
+++ 605_Can_Place_Flowers/dotnet9/attempt2/Solution.cs
@@ -0,0 +1,55 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Collections;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+
+using leetcode;
+
+string PrintList<T>(IList<T> bools)
+{
+    StringBuilder sb = new StringBuilder(bools.Count() * 7 + 2);
+    sb.Append("[");
+    foreach (var v in bools)
+    {
+        sb.Append(v);
+        sb.Append(',');
+    }
+    sb.Append("]");
+
+    return sb.ToString();
+}
+
+string PrintArray<T>(T[] bools) => PrintList<T>(bools.ToList());
+bool Matches(bool[] expected, IList<bool> actual)
+{
+    for (int i = 0; i < expected.Length; i++)
+    {
+        if (expected[i] != actual[i]) return false;
+    }
+    return true;
+}
+
+string PrintTests(Input input, bool expected)
+{
+    Solution solution = new();
+    var processed = solution.CanPlaceFlowers(input.flowerBed, input.n);
+    string status = expected == processed ? "success" : "failure";
+    return $"[{status}] Can place flowers: '{PrintArray(input.flowerBed)}' when given an extra: {input.n} ==> '{processed}'";
+}
+Console.WriteLine(PrintTests(new Input([0], 1), true));
+Console.WriteLine(PrintTests(new Input([1, 0, 0, 0, 1], 1), true));
+Console.WriteLine(PrintTests(new Input([1, 0, 0, 0, 1], 2), false));
+Console.WriteLine(PrintTests(new Input([1, 0, 0, 0, 0, 1], 2), false));
+Console.WriteLine(PrintTests(new Input([0, 0, 0], 2), true));
+Console.WriteLine(PrintTests(new Input([0, 0, 1], 1), true));
+Console.WriteLine(PrintTests(new Input([0, 0, 0, 1], 1), true));
+Console.WriteLine(PrintTests(new Input([0, 0, 0, 1], 2), false));
+Console.WriteLine(PrintTests(new Input([0, 0, 0, 0, 1], 2), true));
+Console.WriteLine(PrintTests(new Input([0, 1, 0], 1), false));
+Console.WriteLine(PrintTests(new Input([0, 1, 0, 0], 1), true));
+Console.WriteLine(PrintTests(new Input([0, 1, 0, 0, 0], 1), true));
+Console.WriteLine(PrintTests(new Input([0, 1, 0, 0, 0, 0], 2), true));
+
+record Input(int[] flowerBed, int n);
