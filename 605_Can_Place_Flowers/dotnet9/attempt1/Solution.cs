53a57dee582903358c1508c641527bd2fafe605c
Gregory Berman <greg@5fn.ai>
2024-12-31 00:20:48 -0500

    completed question 605 Can place flower

---
+++ 605_Can_Place_Flowers/dotnet9/attempt1/Solution.cs
@@ -0,0 +1,43 @@
+﻿namespace leetcode;
+public class Solution
+{
+    public (bool Found, int Index) IndexOf(int[] input, int find, int startingPoint)
+    {
+        for (int i = startingPoint; i < input.Length; i++)
+        {
+            if (input[i] == find) return (true, i);
+        }
+        return (false, 0);
+    }
+    public bool CanPlaceFlowers(int[] flowerbed, int n)
+    {
+        (bool found, int index) = IndexOf(flowerbed, 1, 0);
+        if (found)
+        {
+            n -= index / 2;
+            int startingIndex = index+1;
+            
+            while(true)
+            {
+                (found, index) = IndexOf(flowerbed, 1, startingIndex);
+                if (found)
+                {
+                    var l = index - startingIndex;
+                    n -= l / 2 + (l % 2 - 1);
+
+                    startingIndex = index+1;
+                }
+                else { break; }
+                
+            }
+            int ll = flowerbed.Length - startingIndex;
+            n -= ll / 2;
+        }
+        else
+        {
+            n -= flowerbed.Length / 2 + flowerbed.Length % 2;
+        }
+
+        return n <= 0;
+    }
+}
